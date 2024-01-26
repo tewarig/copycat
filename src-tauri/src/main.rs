459eaae7f5cd6eafec4f3623cfdebec9c780e2b2
tewarig <gauravtewari111@gmail.com>
2024-01-26 17:51:45 +0530

    feat: add basic functions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,8 +8,9 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
-    tauri::Builder::default()
+    tauri::Builder::default().plugin(tauri_plugin_clipboard::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
