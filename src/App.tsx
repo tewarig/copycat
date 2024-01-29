3a7953b112a874c4f86bfdbc2648dc8d0bb12845
tewarig <gauravtewari111@gmail.com>
2024-01-29 21:38:18 +0530

    feat: added always on top popover and basic copy function for text

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { Separator } from "@/components/ui/separator"
+import { Button } from "@/components/ui/button"
+import { FaCopy } from "react-icons/fa";
+
+
+
+
+
 
 import {
   readText,
@@ -55,9 +65,33 @@ function App() {
   []);
 
   return (
-    <div className="container">
-      <h1>Welcome to Copy Cat</h1>
-      <p> A Simple yet powerful clipboard manager</p>
+    <div className="flex-col justify-center items-center p-4">
+      <div className="arrow"></div>
+      <Tabs defaultValue="text" className="w-[400px] mt-4">
+  <TabsList>
+    <TabsTrigger value="text">Text</TabsTrigger>
+    <TabsTrigger value="files">Files</TabsTrigger>
+  </TabsList>
+  <TabsContent value="text"><ScrollArea className="h-[300px] w-[400px] rounded-md border p-4">
+  {
+        localStore?.map((text, index) => (
+          <div className="p-2">
+          <div key={index} className="flex justify-between items-center m-2">
+            <div>{text.substring(0,70)}</div>
+            <Button onClick={() => writeText(text)}><FaCopy /></Button>
+          </div>
+          <Separator />
+          </div>
+
+        ))
+      }
+   
+</ScrollArea>
+</TabsContent>
+  <TabsContent value="files">Change your password here.</TabsContent>
+</Tabs>
+
+    {/* <div className="content">
       {
         localStore?.map((text, index) => (
           <div key={index}>
@@ -66,6 +100,7 @@ function App() {
           </div>
         ))
       }
+    </div> */}
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path"
+
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
@@ -9,6 +11,11 @@ export default defineConfig(async () => ({
   //
   // 1. prevent vite from obscuring rust errors
   clearScreen: false,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
