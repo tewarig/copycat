b4b335f1c2fbd1da44c6c1416f8a96acfcb2b9ea
tewarig <gauravtewari111@gmail.com>
2024-03-17 23:08:17 +0530

    feat: added ui changes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getFileNameFromUrl = (fileName: string): string => {
+  //since filename is a url like something/desktop/file.png we split by /
+  const fileNameArray = fileName.split("/");
+  return fileNameArray[fileNameArray.length - 1].substring(0, 70);
+};
